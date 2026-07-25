1b9fd43b599d17471c09f44c981c8e5f3820d982
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-25 15:55:53 +0300

    Question01

--- Asshgnment05/Program.cs
+++ Asshgnment05/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part01
+            #region Quesion01
+            /*
+             An interface is a contract that defines a set of members without implementation(Signature members).
+             use interfaces instead of depending on concrete classes to achieve abstraction and loose coupling and
+             allows to change implementations without modifying the code that uses them.
+             Benefits of interface : 
+             Enable polymorphism.
+             Reduce tight coupling.
+             Make testing easier.
+             Allow multiple inheritance of behavior  
+             */
+            #endregion
+            #endregion
         }
     }
 }
